199eb7bfe7887fdf7a378fd41e9383c47e329a0f
HannaWang003 <hannawang003@gmail.com>
2024-07-11 11:55:00 +0800

    整理後台表單連結

--- back.php
+++ back.php
@@ -37,35 +37,35 @@ include_once "./api/db.php";
                         <div class="mainmu">
                             網站標題管理 </div>
                     </a>
-                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=admin&redo=ad">
+                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=ad">
                         <div class="mainmu">
                             動態文字廣告管理 </div>
                     </a>
-                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=admin&redo=mvim">
+                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=mvim">
                         <div class="mainmu">
                             動畫圖片管理 </div>
                     </a>
-                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=admin&redo=image">
+                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=image">
                         <div class="mainmu">
                             校園映象資料管理 </div>
                     </a>
-                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=admin&redo=total">
+                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=total">
                         <div class="mainmu">
                             進站總人數管理 </div>
                     </a>
-                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=admin&redo=bottom">
+                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=bottom">
                         <div class="mainmu">
                             頁尾版權資料管理 </div>
                     </a>
-                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=admin&redo=news">
+                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=news">
                         <div class="mainmu">
                             最新消息資料管理 </div>
                     </a>
-                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=admin&redo=admin">
+                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=admin">
                         <div class="mainmu">
                             管理者帳號管理 </div>
                     </a>
-                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=admin&redo=menu">
+                    <a style="color:#000; font-size:13px; text-decoration:none;" href="?do=menu">
                         <div class="mainmu">
                             選單管理 </div>
                     </a>
@@ -74,7 +74,7 @@ include_once "./api/db.php";
                 </div>
                 <div class="dbor" style="margin:3px; width:95%; height:20%; line-height:100px;">
                     <span class="t">進站總人數 :
-                        1 </span>
+                        <?= $Total->find(1)['total'] ?></span>
                 </div>
             </div>
             <div class="di"
