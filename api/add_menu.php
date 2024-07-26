bb5197b78284f848ebef5acc6b1057a2bd8c98ef
HannaWang003 <hannawang003@gmail.com>
2024-07-27 00:37:08 +0800

    init p2

--- api/add_menu.php
+++
@@ -1,4 +0,0 @@
-<?php
-include_once "db.php";
-$Menu->save($_POST);
-to("../back.php?do=menu");
--- api/login.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once "db.php";
-$res = $Admin->count(['acc' => $_POST['acc'], 'pw' => $_POST['pw']]);
-if ($res > 0) {
-    $_SESSION['admin'] = $_POST['acc'];
-    to("../back.php");
-} else {
-    to("../index.php?do=admin&error=帳號或密碼錯誤");
-}
