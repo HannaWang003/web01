aad1415d65a897b81d909483561fa0d0169585f2
HannaWang003 <hannawang003@gmail.com>
2024-09-16 10:06:53 +0800

    p4 init

--- api/add_menu.php
+++
@@ -1,5 +0,0 @@
-<?php
-include_once "db.php";
-$_POST['big_id'] = 0;
-$Menu->save($_POST);
-to("../back.php?do=menu");
--- api/total.php
+++
@@ -1,4 +0,0 @@
-<?php
-include_once "db.php";
-$Total->save(['id' => 1, 'total' => $_POST['total']]);
-to("../back.php?do=total");
