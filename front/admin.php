e581f66bdae6488f1d99650f4f6f5446c195b877
HannaWang003 <hannawang003@gmail.com>
2024-09-17 08:42:59 +0800

    error

---
+++ front/admin.php
@@ -0,0 +1,6 @@
+                                        <!--正中央-->
+                                        <p class="t botli">管理員登入區</p>
+                                        <p class="cent">帳號 ： <input name="acc" autofocus="" type="text"></p>
+                                        <p class="cent">密碼 ： <input name="ps" type="password"></p>
+                                        <p class="cent"><input value="送出" type="submit"><input type="reset" value="清除">
+                                        </p>
